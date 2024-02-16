2eaf63d7bcfc52c025707e377815760c3643f252
djibril Abaltou <95239684+djibril1212@users.noreply.github.com>
2024-02-16 09:25:56 +0100

    Update homepage.vue

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -5,7 +5,7 @@ import Basebutton from "@/components/basebutton.vue";
 <template>
   <div class="homepage-container">
     <div class="homepage">
-      <h1>Homepage</h1>
+     <h1>Welcome to Your Mail Application</h1>
       <p><br> TP 3 <br>Ex4 : Create HomePage component <br> Ex5 : Create BaseHeader and BaseFooter <br> Ex6 : Create BaseLayout that uses slots API <br> Ex7 : Create BaseButton with primary color <br> TP 4 <br> Ex1 : Add SigninButton <br> Ex2 : State management with props/events <br> Ex3 : State management with provide/inject <br> Ex4: State management with a store <br> Ex5: Add vue-router dependency <br> Ex6: Add / and /conversations route <br> Ex7: Add header navigation</p>
       <Basebutton />
     </div>
