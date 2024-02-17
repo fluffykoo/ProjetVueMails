2cc770b22adf987852c7d2bca06177f9592be846
Adrrien <113974245+Adrrien04@users.noreply.github.com>
2024-02-17 14:35:55 +0100

    ajout fonction google login (main)

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './routes/index.js'
 import store from './lib/store.js'
 
+import vue3GoogleLogin from 'vue3-google-login'
+
+
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.use(vue3GoogleLogin, { clientId: '168918217010-4l131cohtfj8t7aomeaclqtdm0d1k2lm.apps.googleusercontent.com' });
 app.mount('#app');
