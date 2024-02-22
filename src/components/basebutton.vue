a9c85785ccc9f96c6359b40be023783b2eba1cdf
djibril Abaltou <95239684+djibril1212@users.noreply.github.com>
2024-02-22 14:09:12 +0100

    Update basebutton.vue

--- src/components/basebutton.vue
+++ src/components/basebutton.vue
@@ -1,20 +1,15 @@
 <script setup>
 </script>
-
 <template>
   <nav class="buttons">
     <button class="button">Click me</button>
     <button class="button" :disabled="isDisabled">Don't Click me</button>
   </nav>
 </template>
-
-
-
 <style>
 .buttons {
   display: flex;
 }
-
 .button {
   background-color: white;
   color: #4caf50;
@@ -25,7 +20,6 @@
   cursor: pointer;
   transition: background-color 0.3s, color 0.3s;
 }
-
 .button:hover {
   background-color: green;
   color: white;
