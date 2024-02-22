10a55a589fd3137af454e5c2dee0310ca83c6b7d
LoaneCTLL <loane.cotellon@efrei.net>
2024-02-22 08:22:35 +0100

    Update ComposeEmail.vue

--- src/components/ComposeEmail.vue
+++ src/components/ComposeEmail.vue
@@ -7,7 +7,6 @@
         v-model="body"
         placeholder="Message"
         class="input-field"
-        
       ></textarea>
       <button type="submit" class="submit-button">Send</button>
     </form>
@@ -24,13 +23,12 @@ export default {
   },
   methods: {
     addEmail() {
-        const email = {
-          subject: this.subject,
-          body: this.body,
-        };
-        this.$store.commit("addEmail", email); // Ajoute l'e-mail dans le store 
-        this.clearFields(); // Réinitialise les champs du formulaire
-      } 
+      const email = {
+        subject: this.subject,
+        body: this.body,
+      };
+      this.$store.commit("addEmail", email); // Ajoute l'e-mail dans le store Vuex
+      this.clearFields(); // Réinitialise les champs du formulaire
     },
     clearFields() {
       this.subject = "";
@@ -41,5 +39,52 @@ export default {
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f2f2f2;
+}
+
+.container {
+  max-width: 800px;
+  margin: 20px auto;
+  padding: 20px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  font-size: 28px;
+  margin-bottom: 20px;
+  color: #333333;
+}
+
+.input-field {
+  margin-bottom: 15px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 100%;
+}
+
+.submit-button {
+  padding: 10px 15px;
+  background-color: #e6466c;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s;
+  width: 100%;
+}
+
+.submit-button:hover {
+  background-color: #45a049;
+}
 </style>
