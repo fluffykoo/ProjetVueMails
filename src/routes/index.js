e86c65098fd69c8adb854a023db5bec6cf984369
Adrrien <113974245+Adrrien04@users.noreply.github.com>
2024-02-22 09:43:20 +0100

    mise a jour du nom de conversationindexpage devenu boitdereception

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 
 import { createRouter, createWebHistory } from 'vue-router';
 import Homepage from '@/components/homepage.vue';
-import ConversationsIndexPage from '@/components/conversationsindexpage.vue';
+import ConversationsIndexPage from '@/components/boitedereception.vue';
 import ComposeEmail from '@/components/ComposeEmail.vue';
 import SentEmails from '@/components/SentEmails.vue';
 
